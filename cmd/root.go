61718cf5e3572747c4a49ece15697620575f3c6e
KennyChen <kennychen851228@gmail.com>
2020-03-16 20:25:39 +0800

    init commit

--- cmd/root.go
+++ cmd/root.go
@@ -3,23 +3,52 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"os"
+	"github.com/spf13/viper"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long:  `A Fast and Flexible Static Site Generator built with
-				love by spf13 and friends in Go.
-				Complete documentation is available at http://kennychen-blog.herokuapp.com`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-	},
+var (
+	// Used for flags
+	cfgFile     string
+	userLicense string
+
+	rootCmd = &cobra.Command{
+		Use:   "cobra",
+		Short: "A generator for Cobra based Applications",
+		Long: `Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	}
+)
+
+func Execute() error {
+	return rootCmd.Execute()
 }
 
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file config file (default is ./.cobra.yaml)")
+	rootCmd.PersistentFlags().StringP("author", "a", "Kenny", "author name for copyright attribution")
+	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
+	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
+	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
+	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
+	viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
+	viper.SetDefault("license", "apache")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.AddConfigPath("./")
+		viper.SetConfigName(".cobra")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/KennyChenFight/cli-tool-cobra
 
 go 1.13
 
-require github.com/spf13/cobra v0.0.6
+require (
+	github.com/spf13/cobra v0.0.6
+	github.com/spf13/viper v1.4.0
+)
